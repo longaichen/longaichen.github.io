d7c793e307381ad814d70a3fcfb6f9e8c06ca609
xuechenglong <1010332909@qq.com>
2017-11-13 15:51:04 +0800

    3

--- js/functions.js
+++ js/functions.js
@@ -110,7 +110,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + 天 + "</span> days <span class=\"digit\">" + 小时 + "</span> hours <span class=\"digit\">" + 分 + "</span> minutes <span class=\"digit\">" + 秒 + "</span> seconds"; 
+	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
 }
 
